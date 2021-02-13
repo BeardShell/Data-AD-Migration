98f2282ed49be7ecc8b15efec73747f012c573d1
BeardShell <78932365+BeardShell@users.noreply.github.com>
2021-02-13 21:34:06 +0100

    Update Data-AD-migration.ps1

--- Data-AD-migration.ps1
+++ Data-AD-migration.ps1
@@ -4,12 +4,39 @@ Import-Module ActiveDirectory -ErrorAction SilentlyContinue
 
 #Set Initial Variables
 $workingDir = "D:\Migratie"         #Basis directory. LET OP: Geen \ op het einde toevoegen!
+$ADSearchBase = "OU=Data Toegang,OU=Groepen,OU=Organisatie Nieuw,DC=lv,DC=leidschendamvoorburg,DC=nl"
 
 #VANAF HIER GEEN AANPASSINGEN MAKEN AAN HET SCRIPT#
 
 $csvDir = "$($workingDir)\Csv"      #locatie voor de export CSV's
 $logDir = "$($workingDir)\Logging"  #locatie voor de logging
 $xmlDir = "$($workingDir)\Xml"      #locatie voor XML bestanden
+
+Function Initialize-Migration {
+    [CmdLetBinding()]
+    Param (
+        [Parameter(ValueFromPipeline=$true,Mandatory=$true)]
+        [string]$ADSearchBase
+    )
+    try {
+        $ADGroups = Get-ADGroup -Filter * -SearchBase $ADSearchBase
+        #Write-Verbose $ADGroups
+        foreach ($ADGroup in $ADGroups) {
+            $fileName = $ADGroup.Name
+            $CSV = $fileName + ".csv"
+            #Write-Output $CSV
+            $ADGroupMembers = Get-ADGroupMember -Identity $ADGroup | foreach {
+                [pscustomobject]@{
+                    GroupName = $ADGroup.Name
+                    Name = $_.SamAccountName
+                }
+            }
+            $ADGroupMembers | Export-Csv -Path "D:\Temp\Csv\$($CSV)" -Delimiter ";"
+        }
+    } Catch {
+        Write-Error $error.Count
+    }
+}
 
 Function Get-MigrateADGroup {
     #haal oude groepen op
